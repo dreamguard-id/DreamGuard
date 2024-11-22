3428b3047aa242ae656f6274d71d8f5918609db8
arqiarkaan <darren.arqiarkaan.dyazfajri.teddy.tik22@mhsw.pnj.ac.id>
2024-11-22 19:54:23 +0700

    add middleware for authorization

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,50 @@
+const { auth } = require('../config/firebase-config');
+
+exports.isAuthenticated = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({
+        status: 'error',
+        message: 'Authorization token is missing or malformed',
+      });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    // ! Mock token buat testing, jangan lupa di delete
+    if (token === 'mock-token') {
+      req.user = {
+        uid: 'beaZcwkk0YhdLWkK7CuHeoz90Lt1',
+      };
+      return next();
+    }
+
+    // Ini verifikasi token dari Firebase yang aslinya
+    const decodedToken = await auth.verifyIdToken(token);
+
+    req.user = {
+      uid: decodedToken.uid,
+      /**
+       * Kalo butuh data lain kaya di bawah ini, bisa diambil dari decodedToken :)
+       */
+      // email: decodedToken.email || null,
+      // emailVerified: decodedToken.email_verified || false,
+      // name: decodedToken.name || null, //
+    };
+    next();
+  } catch (error) {
+    console.error('Error verifying token:', error.message);
+
+    const errorMessage =
+      error.code === 'auth/id-token-expired'
+        ? 'Token has expired'
+        : 'Invalid or expired token';
+
+    res.status(403).json({
+      status: 'error',
+      message: errorMessage,
+    });
+  }
+};
