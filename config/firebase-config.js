175cd5ea2cf03ba612bbadd578e1ea8b77616abd
arqiarkaan <darren.arqiarkaan.dyazfajri.teddy.tik22@mhsw.pnj.ac.id>
2024-11-22 19:53:20 +0700

    add firebase config

---
+++ config/firebase-config.js
@@ -0,0 +1,13 @@
+const admin = require('firebase-admin');
+const { getAuth } = require('firebase-admin/auth');
+const { getFirestore } = require('firebase-admin/firestore');
+const serviceAccountKey = require('./serviceaccountkey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
+
+const auth = getAuth();
+const db = getFirestore();
+
+module.exports = { auth, db, admin };
