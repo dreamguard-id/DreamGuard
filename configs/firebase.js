999113f104a259e987694ee9007aadc9d5b881ae
arqiarkaan <darren.arqiarkaan.dyazfajri.teddy.tik22@mhsw.pnj.ac.id>
2024-11-22 21:34:31 +0700

    changing serviceaccountkey directory

--- configs/firebase.js
+++ configs/firebase.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 const { getAuth } = require('firebase-admin/auth');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccountKey = require('./serviceaccountkey.json');
+const serviceAccountKey = require('../serviceaccountkey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
