945f0511b5db388b11a5443b7c01b2bf4a3d3dfd
arqiarkaan <darren.arqiarkaan.dyazfajri.teddy.tik22@mhsw.pnj.ac.id>
2024-11-23 23:39:11 +0700

    update route name

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
-const profileRoutes = require('./routes/user');
+const userRoutes = require('./routes/user');
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/user', profileRoutes);
+app.use('/api/user', userRoutes);
 
 app.get('/', async (req, res) => {
   res.json({ status: 'Aku siap' });
