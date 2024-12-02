10f6c2fbc3e3ea849893adff06fa399a4b813623
arqiarkaan <darren.arqiarkaan.dyazfajri.teddy.tik22@mhsw.pnj.ac.id>
2024-12-03 00:52:22 +0700

    add new endpoints

--- routes/user.js
+++ routes/user.js
@@ -235,9 +235,12 @@ router.patch(
  */
 function mapPredictionResult(predictionResultId) {
   const resultMapping = {
+    0: 'No Sleep Disorder',
     1: 'Sleep Apnea',
-    2: 'No Sleep Disorder',
-    3: 'Sleep Insomnia',
+    2: 'Sleep Insomnia',
+    3: 'No Sleep Disorder', // ID 3 buat kondisi No Sleep Disorder + Tidur < 8 Jam
+    4: 'No Sleep Disorder', // ID 4 buat kondisi No Sleep Disorder + Stress Level 8-10
+    5: 'No Sleep Disorder', // ID 5 buat kondisi No Sleep Disorder + Tidur < 8 Jam + Stress Level 8-10
   };
   return resultMapping[predictionResultId] || 'Unknown Result';
 }
@@ -321,7 +324,6 @@ router.post('/predictions', isAuthenticated, async (req, res) => {
   }
 });
 
-
 // GET PREDICTIONS HISTORY
 /**
  * * Already tested (Working)
@@ -372,6 +374,116 @@ router.get('/predictions', isAuthenticated, async (req, res) => {
   }
 });
 
+// GET PREDICTIONS BY ID
+/**
+ * * Already tested (Working)
+ */
+router.get('/predictions/:id', isAuthenticated, async (req, res) => {
+  try {
+    const uid = req.user.uid;
+    const predictionId = req.params.id;
+
+    const userDocRef = db.collection('users').doc(uid);
+
+    const userDoc = await userDocRef.get();
+    if (!userDoc.exists) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'User not found.',
+      });
+    }
+
+    const predictionDocRef = userDocRef
+      .collection('predictions')
+      .doc(predictionId);
+    const predictionDoc = await predictionDocRef.get();
+
+    if (!predictionDoc.exists) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Prediction not found.',
+      });
+    }
+
+    const predictionData = predictionDoc.data();
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Prediction details retrieved successfully.',
+      data: {
+        id: predictionDoc.id,
+        ...predictionData,
+      },
+    });
+  } catch (error) {
+    console.error('Error retrieving prediction details:', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to retrieve prediction details.',
+    });
+  }
+});
+
+// FILTER PREDICTIONS BY predictionResultId QUERY PARAMETER
+/**
+ * * Already tested (Working)
+ */
+router.get('/predictions/filter', isAuthenticated, async (req, res) => {
+  try {
+    const uid = req.user.uid;
+    const { predictionResultId } = req.query;
+
+    const userDocRef = db.collection('users').doc(uid);
+
+    const userDoc = await userDocRef.get();
+    if (!userDoc.exists) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'User not found.',
+      });
+    }
+
+    let predictionsQuery = userDocRef
+      .collection('predictions')
+      .orderBy('predictionNumber', 'asc');
+
+    if (predictionResultId) {
+      predictionsQuery = predictionsQuery.where(
+        'predictionResultId',
+        '==',
+        parseInt(predictionResultId, 10)
+      );
+    }
+
+    const predictionsSnapshot = await predictionsQuery.get();
+
+    if (predictionsSnapshot.empty) {
+      return res.status(404).json({
+        status: 'error',
+        message: 'No predictions found for the given filter.',
+      });
+    }
+
+    const predictions = predictionsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      predictionResultId: doc.data().predictionResultId,
+      predictionResultText: doc.data().predictionResultText,
+      createdAt: doc.data().createdAt,
+    }));
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Filtered predictions retrieved successfully.',
+      data: predictions,
+    });
+  } catch (error) {
+    console.error('Error retrieving filtered predictions:', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to retrieve filtered predictions.',
+    });
+  }
+});
 
 // ADD NEW FEEDBACK
 /**
@@ -451,5 +563,254 @@ router.delete('/account', isAuthenticated, async (req, res) => {
     });
   }
 });
+
+// ADD NEW SLEEP SCHEDULE
+function convertTo24HourFormat(timeString) {
+  return DateTime.fromFormat(timeString, 'hh:mm a').toFormat('HH:mm');
+}
+
+function calculateDuration(startTime, endTime) {
+  const start = DateTime.fromFormat(startTime, 'hh:mm a');
+  const end = DateTime.fromFormat(endTime, 'hh:mm a');
+
+  const duration = end.diff(start, ['hours', 'minutes']);
+  const hours = duration.hours;
+  const minutes = duration.minutes;
+
+  return `${hours}h ${minutes}m`;
+}
+
+router.post(
+  '/sleep-schedules',
+  isAuthenticated,
+  [
+    body('bedTime').notEmpty().withMessage('Bedtime is required'),
+    body('wakeUpTime').notEmpty().withMessage('Wakeup time is required'),
+    body('wakeUpAlarm')
+      .isBoolean()
+      .withMessage('Wakeup alarm should be a boolean'),
+    body('sleepReminders')
+      .isBoolean()
+      .withMessage('Sleep reminders should be a boolean'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ status: 'error', errors: errors.array() });
+    }
+
+    const { bedTime, wakeUpTime, wakeUpAlarm, sleepReminders } = req.body;
+
+    try {
+      const uid = req.user.uid;
+      const userDocRef = db.collection('users').doc(uid);
+      const userDoc = await userDocRef.get();
+
+      if (!userDoc.exists) {
+        return res
+          .status(404)
+          .json({ status: 'error', message: 'User not found' });
+      }
+
+      const bedTime24 = convertTo24HourFormat(bedTime);
+      const wakeUpTime24 = convertTo24HourFormat(wakeUpTime);
+
+      const scheduleData = {
+        bedTime,
+        wakeUpTime,
+        wakeUpAlarm,
+        sleepReminders,
+        plannedDuration: calculateDuration(bedTime24, wakeUpTime24),
+        actualBedTime: null,
+        actualWakeUpTime: null,
+        actualDuration: null,
+        difference: null,
+        sleepQuality: null,
+        notes: null,
+        createdAt: DateTime.now()
+          .setZone('Asia/Jakarta')
+          .toFormat('d MMMM yyyy'),
+      };
+
+      const schedulesRef = userDocRef.collection('sleepSchedules');
+      await schedulesRef.add(scheduleData);
+
+      const newScheduleRef = await schedulesRef.add(scheduleData);
+      const id = newScheduleRef.id;
+      await newScheduleRef.update({ id }); // Tambahkan ID ke dokumen
+
+      res.status(201).json({
+        status: 'success',
+        message: 'Sleep schedule created successfully',
+        data: { ...scheduleData, id },
+      });
+    } catch (error) {
+      console.error('Error creating sleep schedule:', error);
+      res
+        .status(500)
+        .json({ status: 'error', message: 'Failed to create sleep schedule' });
+    }
+  }
+);
+
+// GET ALL SLEEP SCHEDULES
+router.get('/sleep-schedules', isAuthenticated, async (req, res) => {
+  try {
+    const uid = req.user.uid;
+    const userDocRef = db.collection('users').doc(uid);
+    const userDoc = await userDocRef.get();
+
+    if (!userDoc.exists) {
+      return res
+        .status(404)
+        .json({ status: 'error', message: 'User not found' });
+    }
+
+    const schedulesSnapshot = await userDocRef
+      .collection('sleepSchedules')
+      .get();
+    if (schedulesSnapshot.empty) {
+      return res
+        .status(404)
+        .json({ status: 'error', message: 'No sleep schedules found' });
+    }
+
+    const schedules = schedulesSnapshot.docs.map((doc) => doc.data());
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Sleep schedules retrieved successfully',
+      data: schedules,
+    });
+  } catch (error) {
+    console.error('Error retrieving sleep schedules:', error);
+    res
+      .status(500)
+      .json({ status: 'error', message: 'Failed to retrieve sleep schedules' });
+  }
+});
+
+
+// UPDATE SLEEP SCHEDULE BY ID
+router.patch(
+  '/sleep-schedules/:id',
+  isAuthenticated,
+  [
+    param('id').isString().withMessage('Invalid schedule ID'),
+    body('bedTime')
+      .optional()
+      .isString()
+      .withMessage('Invalid bed time format'),
+    body('wakeUpTime')
+      .optional()
+      .isString()
+      .withMessage('Invalid wake up time format'),
+    body('wakeUpAlarm')
+      .optional()
+      .isBoolean()
+      .withMessage('Wake up alarm should be a boolean'),
+    body('sleepReminders')
+      .optional()
+      .isBoolean()
+      .withMessage('Sleep reminders should be a boolean'),
+    body('actualBedTime')
+      .optional()
+      .isString()
+      .withMessage('Invalid actual bed time'),
+    body('actualWakeUpTime')
+      .optional()
+      .isString()
+      .withMessage('Invalid actual wake up time'),
+    body('sleepQuality')
+      .optional()
+      .isInt({ min: 1, max: 10 })
+      .withMessage('Sleep quality must be between 1 and 10'),
+    body('notes').optional().isString().withMessage('Invalid notes format'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ status: 'error', errors: errors.array() });
+    }
+
+    const { id } = req.params;
+    const {
+      bedTime,
+      wakeUpTime,
+      wakeUpAlarm,
+      sleepReminders,
+      actualBedTime,
+      actualWakeUpTime,
+      sleepQuality,
+      notes,
+    } = req.body;
+
+    try {
+      const uid = req.user.uid;
+      const userDocRef = db.collection('users').doc(uid);
+      const scheduleRef = userDocRef
+        .collection('sleepSchedules')
+        .doc(id);
+
+      const scheduleDoc = await scheduleRef.get();
+      if (!scheduleDoc.exists) {
+        return res
+          .status(404)
+          .json({ status: 'error', message: 'Schedule not found' });
+      }
+
+      const scheduleData = scheduleDoc.data();
+      const updates = {};
+
+      if (bedTime) updates.bedTime = bedTime;
+      if (wakeUpTime) updates.wakeUpTime = wakeUpTime;
+
+      if (bedTime || wakeUpTime) {
+        const plannedDuration = calculateDuration(bedTime, wakeUpTime);
+        updates.plannedDuration = plannedDuration;
+      }
+
+      if (wakeUpAlarm !== undefined) updates.wakeUpAlarm = wakeUpAlarm;
+      if (sleepReminders !== undefined) updates.sleepReminders = sleepReminders;
+
+      if (actualBedTime && actualWakeUpTime) {
+        const actualBedTime24 = convertTo24HourFormat(actualBedTime);
+        const actualWakeUpTime24 = convertTo24HourFormat(actualWakeUpTime);
+
+        const actualDuration = calculateDuration(
+          actualBedTime24,
+          actualWakeUpTime24
+        );
+        const difference = calculateDuration(
+          scheduleData.plannedDuration,
+          actualDuration
+        );
+
+        updates.actualBedTime = actualBedTime;
+        updates.actualWakeUpTime = actualWakeUpTime;
+        updates.actualDuration = actualDuration;
+        updates.difference = difference;
+      }
+
+      if (sleepQuality) updates.sleepQuality = sleepQuality;
+      if (notes) updates.notes = notes;
+
+      if (Object.keys(updates).length > 0) {
+        await scheduleRef.update(updates);
+      }
+
+      res.status(200).json({
+        status: 'success',
+        message: 'Sleep schedule updated successfully',
+        data: updates,
+      });
+    } catch (error) {
+      console.error('Error updating sleep schedule:', error);
+      res
+        .status(500)
+        .json({ status: 'error', message: 'Failed to update sleep schedule' });
+    }
+  }
+);
 
 module.exports = router;
