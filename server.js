20ebb32522c24011ac6378b1133ff23f9cea81a8
arqiarkaan <darren.arqiarkaan.dyazfajri.teddy.tik22@mhsw.pnj.ac.id>
2024-12-03 20:58:49 +0700

    remove unused routes

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const modelRoutes = require('./routes/model');
 const { db } = require('./configs/firebase');
 const { bucket } = require('./configs/bucket');
 require('dotenv').config();
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/models', modelRoutes);
 
