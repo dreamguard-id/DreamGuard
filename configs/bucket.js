a875d59bc129e70373dac10140b2c87be5ec5752
arqiarkaan <darren.arqiarkaan.dyazfajri.teddy.tik22@mhsw.pnj.ac.id>
2024-12-05 21:11:58 +0700

    update env name

--- configs/bucket.js
+++ configs/bucket.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const storage = new Storage();
 
-const bucketName = process.env.GCLOUD_STORAGE_BUCKET;
+const bucketName = process.env.CLOUD_STORAGE_BUCKET;
 const bucket = storage.bucket(bucketName);
 
 module.exports = { bucket };
